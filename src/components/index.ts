1cbef1da34716aef03074517333c76f35284dcfc
George Kasparyants <gg_plastt@mail.ru>
2026-02-01 10:59:20 +0100

    fix components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,3 @@
 export { default as PieCard } from './PieCard';
-export { default as PieBaseRoot } from './PieBaseRoot';
-export { default as PieStaticRoot } from './PieStaticRoot';
-export { default as PieRoot } from './PieRoot';
 export { default as UI } from './UI';
+export { default as ChatCard } from './Chats/ChatCard';
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-export { PieCard, PieRoot, PieStaticRoot, PieBaseRoot, UI } from './components';
+export { default as UI } from './components/UI'
+export { default as PieBaseRoot } from './components/PieBaseRoot'
+export { default as PieStaticRoot } from './components/PieStaticRoot'
+export { default as PieRoot } from './components/PieRoot'
+export { default as PieCard } from './components/PieCard'
 export { registerPieComponent } from './util/registry'
 
 export type {
