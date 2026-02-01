cfdf95cbeb875fdbd1162503ce6b24d42416c38b
George Kasparyants <gg_plastt@mail.ru>
2026-02-01 15:00:44 +0100

    fix components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,6 @@
 export { default as PieCard } from './PieCard';
 export { default as UI } from './UI';
 export { default as ChatCard } from './Chats/ChatCard';
+export { default as AjaxButtonCard } from './Buttons/AjaxButtonCard'
+export { default as SequenceCard } from './Containers/SequenceCard'
+export { default as UnionCard } from './Containers/UnionCard'
