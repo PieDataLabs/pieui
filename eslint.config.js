6878164518a220e7281710aea86d76963be220f0
George Kasparyants <gg_plastt@mail.ru>
2026-02-15 17:15:10 +0300

    fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -29,6 +29,7 @@ module.exports = [
         },
         rules: {
             '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-empty-object-type': 'off',
         },
     },
 ]
