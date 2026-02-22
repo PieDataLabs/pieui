fc5ea778ad57a28eee22aee3d65cf6c6a37866fc
George Kasparyants <gg_plastt@mail.ru>
2026-02-22 20:03:42 +0300

    add integrations cards

---
+++ src/components/Integrations/FSLLoginCard/types/index.ts
@@ -0,0 +1,22 @@
+import { CSSProperties } from 'react'
+import {PieSimpleComponentProps} from "../../../../types";
+
+export interface FSLLoginCardData {
+    name: string
+
+    title: string
+    errorMessage?: string
+
+    appKey: string
+    redirectUri: string
+    state: string
+    isApp: boolean
+    usePopup: boolean
+
+    iconUrl?: string
+    iconPosition?: 'start' | 'end'
+    sx?: CSSProperties
+}
+
+
+export interface FSLLoginCardProps extends PieSimpleComponentProps<FSLLoginCardData> {}
